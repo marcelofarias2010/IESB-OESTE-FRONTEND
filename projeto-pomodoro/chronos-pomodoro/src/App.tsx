4839a93721c3513f3d236e0696c3deb8deb0e628
Vinicius <vinicius.t.campos@iesb.edu.br>
2026-03-23 21:31:49 -0300

    Mudanças até a prática 14

--- projeto-pomodoro/chronos-pomodoro/src/App.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './styles/theme.css';
+import './styles/global.css';
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Container>
+        <Logo />
+      </Container>
 
-export default App
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      {/* Nossa nova estrutura de formulário */}
+      <Container>
+        <form className='form' action=''>
+          {/* Grupo 1: Label e Input */}
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+
+          {/* Grupo 2: Texto de apoio */}
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          {/* Grupo 3: Ciclos */}
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          {/* Grupo 4: Botão */}
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
+}
