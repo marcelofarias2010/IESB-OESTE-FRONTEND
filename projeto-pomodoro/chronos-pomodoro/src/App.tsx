6d1c5eab9d094ecdbf96444cbcb78d18d3191c50
MatheusLimaby <matheus.l.rodrigues@iesb.edu.br>
2026-03-25 11:14:31 -0300

    Desenvolvimento até a prática 2

--- projeto-pomodoro/chronos-pomodoro/src/App.tsx
+++ projeto-pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,6 +1,12 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
-import { Menu } from './components/Menu'
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer'; // <-- Importado!
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -9,11 +15,44 @@ export function App() {
   return (
     <>
       <Container>
+        <Logo />
+      </Container>
+      <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            {/* Mantivemos apenas o botão principal de Play */}
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+
+      {/* Nosso novo rodapé entra aqui, no seu próprio Container! */}
+      <Container>
+        <Footer />
       </Container>
     </>
   );
